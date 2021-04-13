3966cec5e850700e8d77b2fb1538c06d6612f9b9
Sebastian Rollen <seb@overmu.se>
2021-04-13 13:07:52 -0400

    add repository for timekeeper

--- terraform/aws/ecr.tf
+++ terraform/aws/ecr.tf
@@ -2,3 +2,8 @@ resource "aws_ecr_repository" "tca" {
   name                 = "tca"
   image_tag_mutability = "IMMUTABLE"
 }
+
+resource "aws_ecr_repository" "timekeeper" {
+  name                 = "timekeeper"
+  image_tag_mutability = "IMMUTABLE"
+}
