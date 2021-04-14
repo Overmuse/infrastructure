95633ae28b624177806a0b3d69a4f292791f7afd
Sebastian Rollen <seb@overmu.se>
2021-04-14 15:56:02 -0400

    begin full migration to flux deployments

--- terraform/aws/ecr.tf
+++ terraform/aws/ecr.tf
@@ -1,3 +1,18 @@
+resource "aws_ecr_repository" "alpaca-data-relay" {
+  name                 = "alpaca-data-relay"
+  image_tag_mutability = "IMMUTABLE"
+}
+
+resource "aws_ecr_repository" "order-manager" {
+  name                 = "order-manager"
+  image_tag_mutability = "IMMUTABLE"
+}
+
+resource "aws_ecr_repository" "polygon-data-relay" {
+  name                 = "polygon-data-relay"
+  image_tag_mutability = "IMMUTABLE"
+}
+
 resource "aws_ecr_repository" "tca" {
   name                 = "tca"
   image_tag_mutability = "IMMUTABLE"
@@ -7,3 +22,13 @@ resource "aws_ecr_repository" "timekeeper" {
   name                 = "timekeeper"
   image_tag_mutability = "IMMUTABLE"
 }
+resource "aws_ecr_repository" "trader" {
+  name                 = "trader"
+  image_tag_mutability = "IMMUTABLE"
+}
+
+resource "aws_ecr_repository" "volatility-harvesting" {
+  name                 = "volatility-harvesting"
+  image_tag_mutability = "IMMUTABLE"
+}
+
