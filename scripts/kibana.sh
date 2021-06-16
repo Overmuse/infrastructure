4940ff7a290b9951c549f9f79bdce6c1cf2b2979
Sebastian Rollen <seb@overmu.se>
2021-06-15 20:27:33 -0400

    move logging to the logging namespace

--- scripts/kibana.sh
+++ scripts/kibana.sh
@@ -1,2 +1,2 @@
-KIBANA_POD=$(kubectl get pods -l k8s-app=kibana-logging -n kube-system -o jsonpath='{.items[0].metadata.name}')
-kubectl port-forward $KIBANA_POD -n kube-system 5601:5601
+KIBANA_POD=$(kubectl get pods -l k8s-app=kibana-logging -n logging -o jsonpath='{.items[0].metadata.name}')
+kubectl port-forward $KIBANA_POD -n logging 5601:5601
