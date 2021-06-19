fc5c6b00b292ebbf00e2649aed4f3035dfc26ee4
Sebastian Rollen <seb@overmu.se>
2021-06-18 21:50:12 -0400

    add some policies

--- terraform/aws/ecr.tf
+++ terraform/aws/ecr.tf
@@ -3,9 +3,55 @@ resource "aws_ecr_repository" "alpaca-data-relay" {
   image_tag_mutability = "IMMUTABLE"
 }
 
+resouce "aws_ecr_lifecycle_policy" "alpaca-data-relay-policy" {
+    repository = aws_ecr_repository.alpaca-data-relay.name
+
+    policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Only keep last 3 images",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "imageCountMoreThan",
+        "countNumber": 3
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_ecr_repository" "double-trouble" {
   name                 = "double-trouble"
   image_tag_mutability = "IMMUTABLE"
+}
+
+resouce "aws_ecr_lifecycle_policy" "double-trouble-policy" {
+    repository = aws_ecr_repository.double-trouble.name
+
+    policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Only keep last 3 images",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "imageCountMoreThan",
+        "countNumber": 3
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_ecr_repository" "double-trouble-jl" {
@@ -13,9 +59,55 @@ resource "aws_ecr_repository" "double-trouble-jl" {
   image_tag_mutability = "IMMUTABLE"
 }
 
+resouce "aws_ecr_lifecycle_policy" "double-trouble-jl-policy" {
+    repository = aws_ecr_repository.double-trouble-jl.name
+
+    policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Only keep last 3 images",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "imageCountMoreThan",
+        "countNumber": 3
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_ecr_repository" "jump-diffusion" {
   name                 = "jump-diffusion"
   image_tag_mutability = "IMMUTABLE"
+}
+
+resouce "aws_ecr_lifecycle_policy" "jump-diffusion-policy" {
+    repository = aws_ecr_repository.jump-diffusion.name
+
+    policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Only keep last 3 images",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "imageCountMoreThan",
+        "countNumber": 3
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_ecr_repository" "microprice" {
