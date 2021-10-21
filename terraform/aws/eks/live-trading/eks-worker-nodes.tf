60498bd1cf6d40824b73cd03aece2c6e84ce7163
Sebastian Rollen <seb@overmu.se>
2021-10-21 17:47:06 -0400

    scale up cluster

--- terraform/aws/eks/live-trading/eks-worker-nodes.tf
+++ terraform/aws/eks/live-trading/eks-worker-nodes.tf
@@ -45,8 +45,8 @@ resource "aws_eks_node_group" "live-trading" {
   subnet_ids      = aws_subnet.live-trading[*].id
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
+    desired_size = 3
+    max_size     = 3
     min_size     = 1
   }
 
